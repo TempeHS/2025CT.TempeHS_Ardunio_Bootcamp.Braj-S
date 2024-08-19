d32daf8a888136439aa7b6cd59318e953b0cce8f
Braj Sapkota <braj.sapkota@DETNSW>
2024-08-19 14:15:09 +1000

    Update 04.digitalWrite_digitalRead.ino

--- 04.digitalWrite_digitalRead/04.digitalWrite_digitalRead.ino
+++ 04.digitalWrite_digitalRead/04.digitalWrite_digitalRead.ino
@@ -1,4 +1,4 @@
-/*
+  /*
   Author: 
   
   Learning Intention:
@@ -28,12 +28,15 @@
     https://github.com/TempeHS/TempeHS_Ardunio_Bootcamp/blob/main/04.digitalWrite_digitalRead/Bootcamp-digitalRead.png
     https://github.com/TempeHS/TempeHS_Ardunio_Bootcamp/blob/main/04.digitalWrite_digitalRead/Bootcamp-digitalWrite.png
 */
-
+static unsigned int ledPin = 13;
 
 void setup() {
-  
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
 }
