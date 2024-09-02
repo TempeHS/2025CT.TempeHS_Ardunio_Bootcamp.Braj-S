71b74fe85d7cb8b5adbee3c733bbeb15c5ad932a
Braj Sapkota <braj.sapkota@DETNSW>
2024-09-02 14:11:50 +1000

    Update 07.pulseWidthModulation.ino

--- 07.pulseWidthModulation/07.pulseWidthModulation.ino
+++ 07.pulseWidthModulation/07.pulseWidthModulation.ino
@@ -20,18 +20,24 @@
     https://www.tinkercad.com/things/drNDsMxGMIY?sharecode=D6x4_xTTRl_zGs7B-XGqfbQhG_xgjj3TVS_c-GNTK-c
     https://github.com/TempeHS/TempeHS_Ardunio_Bootcamp/blob/main/07.pulseWidthModulation/Bootcamp-PWMOutput.png
 */
+// C++ code
+//
+//AUTHOR: Braj Sapkota
 
-static unsigned int ledPin = 3;
-static unsigned int analogPin = A3;
-void setup() 
+unsigned static int rLED = 9;
+unsigned static int bLED = 10;
+unsigned static int gLED = 11;
+
+void setup()
 {
-  pinMode(ledPin, OUTPUT);
-  Serial.begin(9600);
-  Serial.println("debug");
+  pinMode(rLED, OUTPUT);
+  pinMode(gLED, OUTPUT);
+  pinMode(bLED, OUTPUT);
 }
 
-void loop() {
-int val = analogRead(A3);
-val = val /4;
-analogWrite (ledPin, val);
+void loop()
+{
+	analogWrite(rLED,255);
+	analogWrite(gLED,255);
+	analogWrite(bLED,255);
 }
