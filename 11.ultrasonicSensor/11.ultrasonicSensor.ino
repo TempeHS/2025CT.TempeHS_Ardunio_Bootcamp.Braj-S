df17da302d26a48efb1e578133156b872ce862f6
Braj Sapkota <braj.sapkota@DETNSW>
2024-09-19 10:33:38 +1000

    Update 11.ultrasonicSensor.ino

--- 11.ultrasonicSensor/11.ultrasonicSensor.ino
+++ 11.ultrasonicSensor/11.ultrasonicSensor.ino
@@ -26,21 +26,50 @@
 #include "Ultrasonic.h"
 #include <Servo.h>
 
-Servo myservo;
 Ultrasonic sensor(5);
+Servo myServo;
 
-unsigned static int servoPin = 7;
-unsigned static int potPin = A2;
+#define servoPin 7
+static signed int potpin = A2;
+unsigned long previousMillis = 0;
+const unsigned long gateInterval = 0;
 
 void setup() {
-  myservo.attach(servoPin);
+  
+  myServo.attach(servoPin);
+  myServo.write(180);
+
   Serial.begin(9600);
+
 }
 
 void loop() {
-  Serial.println(sensor.distanceRead());
 
-  int val = analogRead(potPin);
-  val = map(val, 0, 1023, 180 , 0);
-  myservo.write(val);
+  unsigned long currentMillis = millis();
+  int val = analogRead(potpin);  
+  Serial.println(sensor.distanceRead());
+  
+    if (sensor.distanceRead() <= 30) {    
+    myServo.write(0);
+    previousMillis = currentMillis;
+
+
+
+  } else {
+
+
+
+    if (currentMillis - previousMillis >= gateInterval) {
+
+      myServo.write(180);
+
+    }
+
+  }
+
+  //val = map(val, 0, 1023, 0, 180);
+  //myServo.write(val);
+  
+  //Serial.println(analogRead(potpin));
+
 }
