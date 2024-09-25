43b4ba66ea344039707a32e129d820770a9ba185
Braj Sapkota <braj.sapkota@DETNSW>
2024-09-25 11:33:29 +1000

    Update 14.I2C.ino

--- 14.I2C/14.I2C.ino
+++ 14.I2C/14.I2C.ino
@@ -26,12 +26,34 @@
     Air Pressure Sensor: https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/TempeHS_Sensor_Catalogue/Sensor%20Kit/Air_Pressure_Sensor/Air_Pressure_Sensor.ino
 */
 
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
 #include <Wire.h>
+#endif
 
-void setup() {
 
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);  // High speed I2C
+
+// U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);    //Low spped I2C
+
+void setup(void) {
+  u8g2.begin();
 }
 
-void loop() {
-
+void loop(void) {
+  u8g2.clearBuffer();                   // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
+  u8g2.drawStr(0,10,"The quick brown");    // write something to the internal memory
+  u8g2.drawStr(0,20,"fox jumps over");    // write something to the internal memory
+  u8g2.drawStr(0,30,"the lazy dog");    // write something to the internal memory
+  u8g2.drawStr(0,40,"The quick brown");    // write something to the internal memory
+  u8g2.drawStr(0,50,"fox jumps over");    // write something to the internal memory
+  u8g2.drawStr(0,60,"the lazy dog");    // write something to the internal memory
+  u8g2.sendBuffer();                    // transfer internal memory to the display
+  delay(1000);  
 }
