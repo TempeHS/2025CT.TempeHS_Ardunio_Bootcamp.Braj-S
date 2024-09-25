0d464c354140743da8245e95857e82c1440f4d23
Braj Sapkota <braj.sapkota@DETNSW>
2024-09-25 12:26:54 +1000

    Update functions.ino

--- 14.I2C/functions.ino
+++ 14.I2C/functions.ino
@@ -1,8 +1,8 @@
 bool gameOver () {
 if (y <= 11 || y >= 59 || x <= 1 || x >= 119 )
 {
-  u8g2.setCursor(0, 10);
-  u8g2.print("Game Over");
+  Oled.setCursor(0, 10);
+  Oled.print("Game Over");
   return true;
 }
   else
